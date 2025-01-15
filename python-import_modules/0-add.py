578c3ee7bc5dbb51cf93dddf3af7c08fa4658327
aavly <annavyly@gmail.com>
2025-01-15 22:31:05 +1100

    A2: 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,4 +4,8 @@ from add_0 import add
 a = 1
 b = 2
 
-print("{} + {} = {}".format, a, b, add(a, b))
+print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    import add_0
+    add(a, b)
