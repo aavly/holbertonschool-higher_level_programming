6d5bffce4adb9b3ba91dee4c53805a9b2535343e
aavly <annavyly@gmail.com>
2025-01-15 16:27:59 +1100

    pycodestyle

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def uppercase(str):
-	for a in str:
-		if ord(a) < 123 and ord(a) > 96:
-			a = ord(a) - 32
-			print(chr(a), end="")
-		else:
-				print(a, end="")
-	print("")
+    for a in str:
+        if ord(a) < 123 and ord(a) > 96:
+            a = ord(a) - 32
+            print(chr(a), end="")
+        else:
+            print(a, end="")
+    print("")
+
 
 uppercase("best")
 uppercase("Best School 98 Battery street")
