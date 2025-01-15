90b57ff57f13e72390dc8982abe49766196762b4
aavly <annavyly@gmail.com>
2025-01-15 16:35:00 +1100

    .format to meet requirements

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for a in str:
         if ord(a) < 123 and ord(a) > 96:
-            a = ord(a) - 32
-            print(chr(a), end="")
-        else:
-            print(a, end="")
-    print("")
+            a = chr(ord(a) - 32)
+        result = result + a
+    print("{}".format(result))
 
 
 uppercase("best")
