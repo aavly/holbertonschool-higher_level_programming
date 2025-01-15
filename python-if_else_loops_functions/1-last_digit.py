56032b984b31f3ec8df2ff28b967872b012e7712
aavly <annavyly@gmail.com>
2025-01-15 15:59:37 +1100

    grammatical error

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ lastDigit = abs(number) % 10
 str = "Last digit of"
 
 if lastDigit > 5:
-    print(f"{str} is {number} and is greater than 5")
+    print(f"{str} {number} is {lastDigit} and is greater than 5")
 elif lastDigit == 0:
-    print(f"{str} is {number} and is 0.")
+    print(f"{str} {number} is {lastDigit} and is 0")
 else:
-    print(f"{str} is {number} and is greater than 5")
+    print(f"{str} {number} is {lastDigit} and is greater than 5")
