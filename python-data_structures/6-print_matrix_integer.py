0f452b394ea542bbed2976719f3f13b5c7045793
aavly <annavyly@gmail.com>
2025-01-20 17:23:22 +1100

    {:d}

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for j in range(len(row)):
-            print("{}".format(row[j]), end=' ' if j < len(row) - 1 else "")
+            print("{:d}".format(row[j]), end=' ' if j < len(row) - 1 else "")
         print()
