efee0fa817a1a7bfa76d6924c916f5b520e935ce
aavly <annavyly@gmail.com>
2025-01-20 17:24:42 +1100

    {:d}

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -3,5 +3,5 @@
 def print_list_integer(my_list=[]):
     listLength = len(my_list)
     for i in range(listLength):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
 
