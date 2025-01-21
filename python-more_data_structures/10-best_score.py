3a85622562033fea1fe707a073c14fc9bfe8f57c
aavly <annavyly@gmail.com>
2025-01-22 01:44:21 +1100

    A1: 11. Multiply by using map

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -6,6 +6,13 @@ def best_score(a_dictionary):
         return None
 
     best_score = max(a_dictionary.values(), default=None)
-    for k, v in a_dictionary.items():
-        if v == best_score:
-            return k
+    for key, value in a_dictionary.items():
+        if value == best_score:
+            return key
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
