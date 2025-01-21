c8683daf3c131b6e2b7d8bfb81b11e0e8754d669
aavly <annavyly@gmail.com>
2025-01-21 18:16:01 +1100

    A1: 1. Search and replace

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    square_matrix = [row[:] for row in matrix]
+	new_matrix = list(map(lambda row : list(map(lambda x : x ** 2, row)), matrix))
+	return new_matrix
 
-    for i in range(len(square_matrix)):
-        for j in range(len(square_matrix[i])):
-            square_matrix[i][j] = square_matrix[i][j] ** 2
+    #square_matrix = [row[:] for row in matrix]
 
-    return square_matrix
+    #for i in range(len(square_matrix)):
+        #for j in range(len(square_matrix[i])):
+            #square_matrix[i][j] = square_matrix[i][j] ** 2
+
+    #return square_matrix
