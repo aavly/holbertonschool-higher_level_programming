cfd0f498fd4a406b6006e5c80045ee4ce065d0ce
aavly <annavyly@gmail.com>
2025-01-22 01:18:13 +1100

    A3: return statement instead of print

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -6,4 +6,5 @@ def multiply_by_2(a_dictionary):
 
     for key in sorted(a_dictionary.keys()):
         copy_dictionary[key] = a_dictionary[key] * 2
-        return print(f"{key}: {a_dictionary[key]}")
+
+    return copy_dictionary
