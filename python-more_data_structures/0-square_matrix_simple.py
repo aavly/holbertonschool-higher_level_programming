e889dfdf18d994c2a6f4137e4856ac65e3421d37
aavly <annavyly@gmail.com>
2025-01-21 17:44:10 +1100

    A2: inner for looping for square matrices with varying row lengths

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,7 @@ def square_matrix_simple(matrix=[]):
     square_matrix = [row[:] for row in matrix]
 
     for i in range(len(square_matrix)):
-        for j in range(len(square_matrix)):
+        for j in range(len(square_matrix[i])):
             square_matrix[i][j] = square_matrix[i][j] ** 2
 
     return square_matrix
