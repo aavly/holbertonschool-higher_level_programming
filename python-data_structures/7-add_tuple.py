a18803da4404ad6c63771945e90cb8e5a30492b1
aavly <annavyly@gmail.com>
2025-01-21 13:33:18 +1100

    modified operator for length sum1 >=

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,9 +4,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     listA = [0, 0]
     sum1 = 0
     sum2 = 0
-    if len(tuple_a) > 1:
+    if len(tuple_a) >= 1:
         sum1 += tuple_a[0]
-    if len(tuple_b) > 1:
+    if len(tuple_b) >= 1:
         sum1 += tuple_b[0]
 
     if len(tuple_a) >= 2:
