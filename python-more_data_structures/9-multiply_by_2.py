6545bfe461b218b6b3c197ddc2b44f7e27a9729b
aavly <annavyly@gmail.com>
2025-01-22 01:16:43 +1100

    A2: 9. Multiply by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,8 @@
 
 def multiply_by_2(a_dictionary):
 
+    copy_dictionary = a_dictionary.copy()
+
     for key in sorted(a_dictionary.keys()):
-        a_dictionary[key] = a_dictionary[key] * 2
-        print(f"{key}: {a_dictionary[key]}")
+        copy_dictionary[key] = a_dictionary[key] * 2
+        return print(f"{key}: {a_dictionary[key]}")
