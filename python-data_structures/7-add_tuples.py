e41dd9dbf285fc91b43f8dff6fe1638ae1f43b4e
aavly <annavyly@gmail.com>
2025-01-21 13:31:49 +1100

    A1: 7. Tuples addition

--- python-data_structures/7-add_tuples.py
+++ python-data_structures/7-add_tuples.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+    listA = [0, 0]
+    sum1 = 0
+    sum2 = 0
+    if len(tuple_a) > 1:
+        sum1 += tuple_a[0]
+    if len(tuple_b) > 1:
+        sum1 += tuple_b[0]
 
-    first = aList[0] + bList[0]
-    second = bList[1] + bList[1]
+    if len(tuple_a) >= 2:
+        sum2 += tuple_a[1]
+    if len(tuple_b) >= 2:
+        sum2 += tuple_b[1]
 
-    return first, second
-
-
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+    return sum1, sum2
