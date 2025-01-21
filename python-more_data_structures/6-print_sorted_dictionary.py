dcc0a2af6381cf77628d951ccd4b5fe85c9fcc98
aavly <annavyly@gmail.com>
2025-01-22 00:25:59 +1100

    fixing print statement

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 
 def print_sorted_dictionary(a_dictionary):
 
-    for key, value in sorted(a_dictionary.keys()):
-        print(f"{key}: {value}")
+    for key in sorted(a_dictionary.keys()):
+        print(f"{key}: {a_dictionary[key]}")
