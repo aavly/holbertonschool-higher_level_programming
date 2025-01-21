4d6fd8b619e21a9b1697c3d5b72662b3286dc53c
aavly <annavyly@gmail.com>
2025-01-22 00:43:04 +1100

    A3: adding a for loop to check if key exists

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
+    
+    for key in a_dictionary:
+        if key:
+            a_dictionary.pop(key)
 
-	a_dictionary.pop(key)
-
-	return a_dictionary
+    return a_dictionary
