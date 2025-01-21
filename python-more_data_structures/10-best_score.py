8da6389800d7fb2756281fe2029094d24ea4c8ea
aavly <annavyly@gmail.com>
2025-01-22 01:28:07 +1100

    A3: assigned VALUE not key to biggest as a starting point

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -6,7 +6,7 @@ def best_score(a_dictionary):
         return None
 
     # access first key and assign to biggest
-    biggest = next(iter(a_dictionary))
+    biggest = next(iter(a_dictionary.values()))
 
     for key in a_dictionary:
         if a_dictionary[key] > biggest:
