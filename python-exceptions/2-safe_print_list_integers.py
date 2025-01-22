8800d13f566b586c9608cc2992601358ded25268
aavly <annavyly@gmail.com>
2025-01-22 19:37:43 +1100

    A2: edited formatting of output - moved end= outside of format()

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
             if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i], end=""))
+                print("{:d}".format(my_list[i]), end="")
                 printed_elements += 1
     except IndexError:
         print("list index out of range")
