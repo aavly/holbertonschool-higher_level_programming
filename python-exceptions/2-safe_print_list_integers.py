7e20e7a569c9f5624bc1826309ae5ebe1d02a5b5
aavly <annavyly@gmail.com>
2025-01-22 19:48:29 +1100

    A5: Index error to raise instead to break to produce traceback msg

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -11,8 +11,8 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             continue
         except IndexError:
-            break
+            raise
         
-    print("")
+    print()
 
     return printed_elements
