df24670a22ce9d64d91fd127cf6d2f9ff2ac0061
aavly <annavyly@gmail.com>
2025-01-22 19:39:21 +1100

    A3: modifying exception error messages

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -11,6 +11,8 @@ def safe_print_list_integers(my_list=[], x=0):
                 printed_elements += 1
     except IndexError:
         print("list index out of range")
+    except Exception as e:
+        print(e)
     finally:
         print("")
 
