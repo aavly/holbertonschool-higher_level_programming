9801d0337895e9eafbcfba85dc0c167f2f88df04
aavly <annavyly@gmail.com>
2025-01-22 19:45:16 +1100

    A4: modifying exception and try block

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,17 +3,16 @@
 def safe_print_list_integers(my_list=[], x=0):
 
     printed_elements = 0
-
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                printed_elements += 1
-    except IndexError:
-        print("list index out of range")
-    except Exception as e:
-        print(e)
-    finally:
-        print("")
+    
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            printed_elements += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            break
+        
+    print("")
 
     return printed_elements
