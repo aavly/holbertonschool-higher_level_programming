ec48c70dc8390873c90c275b1c5105b11e084c47
aavly <annavyly@gmail.com>
2025-01-26 22:14:11 +1100

    A1: 3. Print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """
+    Prints a square with the character #.
+
+    Args:
+        size: size length of the square.
+
+    Returns:
+        A square in length 'size' of character #.
+
+    Raises:
+        TypeError:	size must be an integer.
+        ValueError: size must be >= 0.
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
