4a5e8d07187237e9df35c15cd4af96474875c1e0
aavly <annavyly@gmail.com>
2025-01-28 10:12:59 +1100

    corrected whitespace on line 10

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,7 +7,7 @@ class Square:
     Class to define a Square based on 0-square.py
 
     Private instance attribute: size
- 
+
     Instantiation with size (no type/value verification)
     """
     def __init__(self, size):
