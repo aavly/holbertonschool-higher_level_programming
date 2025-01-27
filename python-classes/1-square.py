4558bdc238e8997f7e0675ada7d3860d073840c2
aavly <annavyly@gmail.com>
2025-01-27 14:38:10 +1100

    A1: 1. Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """
+    Class to define a Square based on 0-square.py
+
+	Private instance attribute: size
+ 
+	Instantiation with size (no type/value verification)
+    """
+    def __init__(self, size):
+        self.size = size
