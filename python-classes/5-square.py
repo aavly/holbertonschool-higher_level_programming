da0e19b525a98939a128ab70bf5bb3c1f7f9cd96
aavly <annavyly@gmail.com>
2025-01-28 11:25:00 +1100

    rearranging print statements

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -33,9 +33,9 @@ class Square:
         if self.size == 0:
             print()
         for i in range(self.size):
-            print('#' * self.size, end="")
             for j in range(self.size):
-                print()
+                print('#' * self.size, end="")
+            print()
 
     # Getter
     @property
