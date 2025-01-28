bc8452942ec38029f2d28d96ed2366bafb742851
aavly <annavyly@gmail.com>
2025-01-28 14:15:26 +1100

    added getters and setters for position attribute

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -20,20 +20,6 @@ class Square:
         ValueError: size must be >= 0
     """
 
-
-class Square:
-    """
-    Class to define a Square based on 4-square.py
-
-    Private instance attribute: size
-        property def size(self) to retrieve it - getter
-        property def size (self, value) - setter
-
-    Raises:
-        TypeError: size must be an integer
-        ValueError: size must be >= 0
-    """
-
     # Instantiation with optional size
     def __init__(self, size=0, position=(0, 0)):
         if not isinstance(size, int):
@@ -62,23 +48,27 @@ class Square:
         for y in range(self.__position[1]):
             print()
         for i in range(self.__size):
-            #printing spaces for x axis
+            # printing spaces for x axis
             for x in range(self.__position[0]):
                 print(" ", end="")
-                for l in range(self.__size):
+                for j in range(self.__size):
                     print('#', end="")
                 print()
 
-    # Getter
+    # GETTERS
     @property
     def size(self):
         return self.__size
 
-    # Setter
+    @property
+    def position(self):
+        return self.__position
+
+    # SETTERS
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
         self.__size = value
+
+    @position.setter
+    def position(self, value):
+        self.__position = value
