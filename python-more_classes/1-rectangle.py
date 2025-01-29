66eba08e6c564b8185741a635a169dcf6db0e77c
aavly <annavyly@gmail.com>
2025-01-29 18:09:44 +1100

    i just realised i was working on the wrong file

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,6 +11,13 @@ class Rectangle:
 
     Private instance attribute: width
                                 height
+                                
+    Raises:
+        TypeError:	width must be an integer
+        			height must be an integer
+           
+        ValueError: width must be >= 0
+					height must be >= 0
     """
 
     def __init__(self, width=0, height=0):
@@ -42,7 +49,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >=")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
@@ -50,5 +57,5 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >=")
+            raise ValueError("height must be >= 0")
         self.__height = value
