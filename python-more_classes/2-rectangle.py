af6ff7ef917c571cf9f8ea2a9130df217a5d8a12
aavly <annavyly@gmail.com>
2025-01-29 18:01:19 +1100

    added more details to docstrings

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Write a class Rectangle that defines a rectangle by:
+(based on 0-rectangle.py)
+"""
+
+
+class Rectangle:
+    """
+    Class to define Rectangle.
+
+    Private instance attribute: width
+                                height
+                                
+    Raises:
+        TypeError:	width must be an integer
+        			height must be an integer
+           
+        ValueError: width must be >= 0
+					height must be >= 0
+    """
+
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__width = width
+        self.__height = height
+
+    # Getters
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    # Setters
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >=")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >=")
+        self.__width = value
