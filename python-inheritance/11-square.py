03081e113fbfd6064df3c01d6c1f5e12a8fc6278
aavly <annavyly@gmail.com>
2025-02-03 16:18:57 +1100

    A1: 11. Square #2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Write a class Square that inherits from Rectangle (9-rectangle.py).
+(task based on 10-square.py).)
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class to define Square that inherits Rectangle.
+
+    Private attributes:	size (int)
+
+    Raises:
+        Exception:	area() is not implemented
+        TypeError:	<name> must be an integer
+        ValueError:	<name> must be greater than 0
+    """
+    def __init__(self, size):
+        super().integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
