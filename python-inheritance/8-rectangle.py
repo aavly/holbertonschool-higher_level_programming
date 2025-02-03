c2e8c7f88eefacc09f093b4f4553c97446c0568d
aavly <annavyly@gmail.com>
2025-02-03 16:02:34 +1100

    A2: including import statement and using super()

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,31 +4,15 @@ Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
 """
 
 
-class BaseGeometry():
-    """
-    Class to define BaseGeometry.
-
-    Raises:
-        Exception:	area() is not implemented
-        TypeError: <name> must be an integer
-        ValueError: <name> must be greater than 0
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
-        if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """
     Class to define Rectangle that inherits BaseGeometry.
 
-    Private attributes:	width
-                        height
+    Private attributes:	width (int)
+                        height (int)
 
     Raises:
         Exception:	area() is not implemented
@@ -38,11 +22,13 @@ class Rectangle(BaseGeometry):
         ValueError:	<name> must be greater than 0
     """
     def __init__(self, width, height):
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
         self.__width = width
         self.__height = height
-
-    def integer_validator(self, width, height):
-        if not isinstance(self.__width, int):
-            raise TypeError("width must be an intger")
-        if not isinstance(self.__height, int):
-            raise TypeError("height must be an intger")
