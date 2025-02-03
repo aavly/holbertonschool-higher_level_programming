e847f7a4ab5a1b0a3a79bcbb86a47ea2dba6a7e7
aavly <annavyly@gmail.com>
2025-02-03 14:42:17 +1100

    modified code to shortened

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,11 +8,7 @@ from the specified class ; otherwise False
 
 def inherits_from(obj, a_class):
     """
-    Function that checks if obj is an instance of a_class
-    that is inherited from the specifi
+    Function that checks if obj is an instance of a class
+    that is inherited from a_class (directly or indirectly).
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    else:
-        return False
-
+    return isinstance(obj, a_class) and type(obj) != a_class
