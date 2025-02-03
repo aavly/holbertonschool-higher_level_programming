b8b9f3d00c68fed346d26180eb0a56c456cbabc7
aavly <annavyly@gmail.com>
2025-02-03 16:07:48 +1100

    A1: 9. Full rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
+(task based on 8-rectangle.py)
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class to define Rectangle that inherits BaseGeometry.
+
+    Private attributes:	width (int)
+                        height (int)
+
+    Raises:
+        Exception:	area() is not implemented
+        TypeError:	<name> must be an integer
+                    height must be an integer
+                    width must be an integer
+        ValueError:	<name> must be greater than 0
+    """
+    def __init__(self, width, height):
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
