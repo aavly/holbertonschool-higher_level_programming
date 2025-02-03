0a27a664441d15e0ea1fb776dbcf4dc328bd0090
aavly <annavyly@gmail.com>
2025-02-03 14:44:37 +1100

    modified return statement to use isinstance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,4 +11,4 @@ def inherits_from(obj, a_class):
     Function that checks if obj is an instance of a class
     that is inherited from a_class (directly or indirectly).
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and not isinstance(obj, obj.__class__)
