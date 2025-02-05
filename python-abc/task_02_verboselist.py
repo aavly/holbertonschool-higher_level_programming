6f4f665cfc2c1b84525049f42e602995054db05c
aavly <annavyly@gmail.com>
2025-02-05 20:46:24 +1100

    modified pop override

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -5,36 +5,28 @@
 class VerboseList(list):
     """
     Class VerboseList that extends the Python 'list' class.
-    VerboseList should print a notif msg everytime an item is 
+    VerboseList should print a notif msg everytime an item is
     added or removed.
     """
-    def __init__(self, list=[]):
-        self.__list = list
 
     def append(self, item):
+        print("Added [{}] to the list".format(item))
         super().append(item)
-        print("Added {} to the list".format(item))
 
     def extend(self, list_to_add):
+        print("Extended the list with [{}] items".format(len(list_to_add)))
         super().extend(list_to_add)
-        print("Extended the list with {} items".format(len(list_to_add)))
 
     def remove(self, item):
-        if item in self.__list:
-            print("Removed {} from the list".format(item))
-            super().remove(list)
+        if item in self:
+            print("Removed [{}] from the list".format(item))
+            super().remove(item)
 
     def pop(self, item=None):
-        if item in self.__list:
-            print("Popped {} from the list".format(item))
-            super().pop(item)
-        if len(self.__list) > 0 or item is None:
-            super().pop()
-
-# Testing
-vl = VerboseList([1, 2, 3])
-vl.append(4)
-vl.extend([5, 6])
-vl.remove(2)
-vl.pop()
-vl.pop(0)
+        if item is None:
+            to_pop = super().pop()
+            print("Popped [{}] from the list".format(item))
+        else:
+            to_pop = super().pop(item)
+            print("Popped [{}] from the list".format(item))
+        return item
