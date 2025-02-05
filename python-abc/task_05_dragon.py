47ddd7df9183692ba7a57c8f05e10357c189c6db
aavly <annavyly@gmail.com>
2025-02-05 21:35:24 +1100

    A1: 5. The Mystical Dragon - Mastering Mixins

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+5. The Mystical Dragon - Mastering Mixins
+"""
+
+
+class SwimMixin():
+    """
+    Mixin class
+    """
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin():
+    """
+    Mixin class
+    """
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """
+    Dragon class
+    """
+    def roar(self):
+        print("This dragon roars!")
