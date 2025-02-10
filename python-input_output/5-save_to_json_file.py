f701e27c48e950783c3e30c2840acca6e4a3068d
aavly <annavyly@gmail.com>
2025-02-10 19:27:43 +1100

    changed mode in with statement

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -13,8 +13,5 @@ def save_to_json_file(my_obj, filename):
     file, using a JSON representation:
     """
 
-    # Serializing json
-    json_obj = json.dumps(my_obj)
-
-    with open(filename, 'r', encoding="utf-8") as f:
-        json.dumps(json_obj)
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
