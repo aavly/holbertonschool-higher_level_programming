dda36827916ec14204700805bac4b0c61179a234
aavly <annavyly@gmail.com>
2025-02-10 19:25:49 +1100

    using dumps() instead of dump()

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -12,5 +12,9 @@ def save_to_json_file(my_obj, filename):
     Write a function that writes an Object to a text
     file, using a JSON representation:
     """
+
+    # Serializing json
+    json_obj = json.dumps(my_obj)
+
     with open(filename, 'r', encoding="utf-8") as f:
-        json.dump(my_obj, f)
+        json.dumps(json_obj)
