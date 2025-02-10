5b5e60e1b8098ebf17e79db0af984257c2962262
aavly <annavyly@gmail.com>
2025-02-10 17:15:28 +1100

    A2: correcting mode when opening file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,7 +8,7 @@ def write_file(filename="", text=""):
     """
     Writes a string to a text file (UTF8) and returns number of chars written
     """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         if not isinstance(text, str):
             s = str(text)
             return f.write(s)
