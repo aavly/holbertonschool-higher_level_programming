4af71b48d166d086b37ac1788897041c491b9bd8
aavly <annavyly@gmail.com>
2025-02-11 00:01:23 +1100

    A1 - 8. Class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+8. Class to JSON
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    Write a function that returns the dictionary
+    description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object:
+    """
+    return json.dumps(obj.__dict__)
