075a1a06bbe271e12046e5660abf4e02bb3d2c87
aavly <annavyly@gmail.com>
2025-02-10 16:14:53 +1100

    A1 - 0. Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a function that reads a text file (UTF8) and prints it to stdout
+"""
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+    """
+    with open(filename, encoding="utf=8") as f:
+        read_file = f.read()
+
+    print(read_file)
