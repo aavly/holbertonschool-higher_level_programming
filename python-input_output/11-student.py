ae511c4b721ea80eec5aa2229f3b229dbf386976
aavly <annavyly@gmail.com>
2025-02-12 16:36:20 +1100

    11. Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+11. Student to disk and reload
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            return {key: self.__dict__[key]
+                    for key in attrs if key in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        json.load(json)
