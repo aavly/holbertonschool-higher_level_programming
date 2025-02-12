c10bc1daa7f46756d8429b13e23422b1d46b1f91
aavly <annavyly@gmail.com>
2025-02-12 17:44:50 +1100

    changed pickle for json

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -2,15 +2,15 @@
 """
 0. Basic Serialization
 """
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
-    with open(filename, "wb") as file:
-        pickle.dump(data, file)
+    with open(filename, "w") as file:
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
-    with open(filename, "rb") as file:
-        loaded_data = pickle.load(file)
+    with open(filename, "r") as file:
+        loaded_data = json.load(file)
     return loaded_data
