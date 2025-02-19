c9895791cd0f6b1ec906a586577aac027cdff4ec
aavly <annavyly@gmail.com>
2025-02-19 21:05:40 +1100

    small typo in this is a simple API

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -16,7 +16,7 @@ class CustomerRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Hello, this is simple API!")
+            self.wfile.write(b"Hello, this is a simple API!")
 
         elif self.path == '/data':
             self.send_response(200)
