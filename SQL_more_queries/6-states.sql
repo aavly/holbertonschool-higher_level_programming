e53ff5bbfa002a7751e2776758286133bcc18137
aavly <annavyly@gmail.com>
2025-03-09 23:30:03 +1100

    Correcting auto generating constraint to AUTO_INCREMENT

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- 6. States table
--- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-	id INTEGER UNIQUE NOT NULL PRIMARY KEY AUTO GENERATED,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INTEGER UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL
 );
