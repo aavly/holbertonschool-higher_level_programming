4592ebf37442069ea7586828ad8497d3842564ce
aavly <annavyly@gmail.com>
2025-03-09 12:35:42 +1100

    surrounded database name with single quotes

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- 2. Read user
-CREATE DATABASE IF NOT EXISTS htbn_0d_2;
+CREATE DATABASE IF NOT EXISTS 'htbn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
