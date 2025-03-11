fd8cce3fe16e83736d656eb15e70b2e5527cca97
aavly <annavyly@gmail.com>
2025-03-12 10:25:02 +1100

    INNER JOIN with state_id and id

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- 9. Cities by States 
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id=state.id
+JOIN states ON cities.state_id=states.id
 ORDER BY cities.id;
 
