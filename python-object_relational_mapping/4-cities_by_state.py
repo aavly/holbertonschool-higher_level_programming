9ef069db48e868ea5efc1c820310f650c17719d6
aavly <annavyly@gmail.com>
2025-03-18 11:30:07 +1100

    4. Cities by states

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 4. Cities by states """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    user_input_state_name = sys.argv[4]
+
+    # PARAMETISED QUERIES TO PROJECT FROM SQL INJECTIONS
+    query = "SELECT * FROM states ORDER BY id"
+    cur.execute(query, (user_input_state_name,))
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
+    cur.close()
+    db.close()
