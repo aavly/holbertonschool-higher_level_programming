57366b588b8c6b1bdb2ba1d3108b755b037ad7b0
aavly <annavyly@gmail.com>
2025-03-18 13:25:41 +1100

    added a commit() statement to save changes

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -8,21 +8,22 @@ from model_state import State, Base
 
 if __name__ == "__main__":
 
-    # create an enginer to connect to MySQL
     database_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
     engine = create_engine(database_uri)
 
-    # bind the engine to the session
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # adding a state
     new_state = State(name='Louisiana')
     session.add(new_state)
+    
+    # REMEMBER TO SAVE CHANGES TO DATABASE
+    session.commit()
 
     # print results
     print(new_state.id)
 
-    # close session
+
     session.close()
