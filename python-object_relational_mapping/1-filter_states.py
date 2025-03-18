8818eb8fd52c7d3ab90ae0ffe9784476068b15af
aavly <annavyly@gmail.com>
2025-03-18 11:14:48 +1100

    1. Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""0. Get all states """
+""" 1. Filter states """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC") 
-rows = cur.fetchall()
-for r in rows:
-    print(r)
-cur.close()
-db.close()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
+    cur.close()
+    db.close()
