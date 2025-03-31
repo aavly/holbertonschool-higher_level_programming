a7d2b3da970ec736b9fa2b10f38b943e9ea05fd0
aavly <annavyly@gmail.com>
2025-03-31 18:17:06 +1100

    removing tests

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -50,20 +50,3 @@ def generate_invitations(template, attendees):
                 f.write(invitation)
         except IOError as e:
                 print(f"ERROR: Failed to write to {output_filename}. Error : {e}")
-        
-        
-# Testing program
-if __name__ == "__main__":
-# Read template from template.txt
-    with open('template.txt', 'r') as file:
-        template_content = file.read()
-
-# List of attendees
-attendees = [
-    {"name": "Alice", "event_title": "Python Conference", "event_date": "2023-07-15", "event_location": "New York"},
-    {"name": "Bob", "event_title": "Data Science Workshop", "event_date": "2023-08-20", "event_location": "San Francisco"},
-    {"name": "Charlie", "event_title": "AI Summit", "event_date": None, "event_location": "Boston"}
-]
-
-# Call the function with the template and attendees list
-generate_invitations(template_content, attendees)
