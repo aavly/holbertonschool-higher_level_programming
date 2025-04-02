1d41155d51306617aa83daca61129bf0b27b49c7
aavly <annavyly@gmail.com>
2025-04-02 13:52:27 +1100

    tasks 0 and 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+red = document.getElementById("red_header");
+
+red.addEventListener("click", function(){
+	document.querySelector("header").style.color= "#FF0000";
+})
---
+++ javascript-dom_manipulation/README.md
@@ -0,0 +1,1 @@
+Holberton, Sprint 2: JavaScript DOM Manipulation
